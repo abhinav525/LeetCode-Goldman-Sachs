ded39c1eeac715fcfad2fb2a378f5f7a62d30d45
ABHINAV <78312043+abhinav525@users.noreply.github.com>
2023-01-07 17:02:40 +0530

    Create 2212. Maximum Points in an Archery Competition.cpp

---
+++ 2212. Maximum Points in an Archery Competition.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> maximumBobPoints(int numArrows, vector<int>& aliceArrows) {
+        vector<int> ans;
+        int score=0;
+        for(int i=0;i<4096;i++)//2^12=4096
+        {
+            int arrows=numArrows;
+            vector<int> temp(12,0);
+            for(int j=11;j>0;j--){
+                if(i & (1<<j))
+                {
+                    if(arrows<=aliceArrows[j])
+                     continue;
+                    arrows-=(aliceArrows[j]+1);
+                    temp[j]=aliceArrows[j]+1;
+                }
+            }
+            for(int j=0;j<12;j++)
+              if(i & (1<<j)){
+                  temp[j]+=arrows;
+                  break;
+              }
+            int b=0;
+            for(int j=0;j<12;j++)
+             if(temp[j]>aliceArrows[j]) b+=(j);
+            if(b>score) score=b,ans=temp;
+        }
+        return ans;
+        
+    }
+};
